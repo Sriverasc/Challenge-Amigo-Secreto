4c1337eb7a78c65a0dfa6be65d0c23acf56fdb20
Sriverasc <sriverasc18000@gmail.com>
2025-07-29 15:11:52 -0600

    Feat: Agregar nombres validados

--- app.js
+++ app.js
@@ -1,1 +1,46 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+const nombres = []; // Array para almacenar los nombres de los amigos
+const li = document.getElementById("listaAmigos"); // Se obtiene el elemento de la lista donde se agregarán los nombres
+const nombreFormatoHTML =
+`<ul class="name-item" role="listitem">
+    <div class="name-text">
+        nombre-placeholder
+    </div>
+    <div class="button-remove" onclick="eliminarAmigo(this)">
+        <i class="material-icons">do_not_disturb_on</i>
+    </div>
+</ul>`; // Plantilla HTML para cada nombre
+
+const validarNombre = (nombre) => {
+    const regex = /^[a-zA-ZÀ-ÿ\s]+$/; // Permite letras, acentos y espacios
+    
+    // Se valida que el nombre no esté vacío
+    if (!nombre) {
+        alert("El nombre no puede estar vacío");
+        return false;
+    }
+
+    // Se valida que el nombre solo tenga letras y espacios
+    if (!regex.test(nombre)) {
+        alert("El nombre solo puede contener letras y espacios");
+        return false;
+    }
+
+    // Se valida que el nombre no este repetido
+    if (nombres.includes(nombre)) {
+        alert("El nombre ya existe");
+        return false;
+    }
+    return true;
+}
+
+const agregarAmigo = () => {
+    const nombreInput = document.getElementById("amigo").value.trim(); // Se obtiene el valor del input y se elimina espacios al inicio y al final
+
+    if (validarNombre(nombreInput)) { // Se valida el nombre
+        nombres.push(nombreInput); // Se agrega el nombre al array
+        const nombreHTML = nombreFormatoHTML.replace("nombre-placeholder", nombreInput); // Se reemplaza el placeholder por el nombre ingresado
+        li.innerHTML += nombreHTML; // Se agrega el nombre al HTML
+        document.getElementById("amigo").value = ""; // Se limpia el input
+    }
+}
